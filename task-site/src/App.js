04c6e798be2952943a92df80c104c62435d7c551
gapgustavo <gapgustavo.gap@gmail.com>
2023-08-01 15:33:32 -0300

    adding REACT engine

---
+++ task-site/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = 'potato'
+  return (
+    <div className="App">
+    <h1>My app {name}</h1>
+    <p>initial</p>
+    </div>
+  );
+}
+
+export default App;
